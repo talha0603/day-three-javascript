732df118a6bf3338c31c46957f8e1e4c6fe8f1cf
talha0603 <gremmi.gremii003@gmail.com>
2026-01-12 17:01:21 -0800

    conversion of datatypes

---
+++ conversionOperation.js
@@ -0,0 +1,38 @@
+let score = "40abcd"
+let score1 = "Talha"
+/*
+console.log(typeof score);
+console.log(typeof (score));
+console.log(score);
+*/
+//console.log(typeof null);
+//console.log(score1);
+
+
+
+/*
+let conversionInNumber = Number(score)   // converted into number
+console.log(typeof conversionInNumber);
+console.log(conversionInNumber);         // it showed NaN, not "40abcd"
+ */
+let conversionInNumber1 = Number(score1)
+//console.log(typeof conversionInNumber1);
+//console.log(conversionInNumber1);
+
+
+// "40" => 33
+// "40abcd" => NaN
+// true = 1 ; false = 0
+
+let isLoggedIn = "Talha"
+let newVariable = Boolean(isLoggedIn)
+//console.log(newVariable);
+
+// 1 => true ; 0 => false
+// "" => false
+// "Talha" => true
+
+let anyNumber = 33
+let stringNumber = String(anyNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
