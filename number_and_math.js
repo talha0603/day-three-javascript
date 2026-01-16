76b2af8c47efadc7906e73c5a703b8993a14a5bb
talha0603 <gremmi.gremii003@gmail.com>
2026-01-16 02:45:41 -0800

    maths and numbers

---
+++ number_and_math.js
@@ -0,0 +1,15 @@
+const marks = 1022
+console.log(marks);
+
+const leftMoney = new Number(30000)
+console.log(leftMoney);
+
+console.log(leftMoney.toString())      // convert number to string
+console.log(leftMoney.toString().length)
+
+console.log(leftMoney.toFixed(2));     // output is 30000.00 & is precised
+
+const newBalance = 45.674
+console.log(newBalance.toPrecision(3));  // we have to set precison value upto '1 to 21'
+
+
